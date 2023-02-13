ee495efd5a7b0664b48ec553fb6cb5d15f04fae0
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-13 12:24:12 +0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,29 +8,33 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 //Styles
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
   return (
-    // <div className="App">
-    //   <div className="list-container">
-    //   </div>
-    // </div>
     <Container fluid className="App">
-    <Row className="title-area">
-      <h1>&#128221; To Do List in React &#128221;</h1>
-    </Row>
-    <Row className="list-area">
-      <Container>
-        <Row className="date-time">
-          <Col></Col>
-        </Row>
-      </Container>
-    </Row>
-    <Row className="credits-area">
-    </Row>
+      <Row className="title-area">
+        <h1>&#128221; To Do List in React &#128221;</h1>
+      </Row>
+      <Row className="list-area">
+        <Container>
+          <Row className="date-time">
+            <Col>
+              <p>1</p>
+            </Col>
+            <Col>
+              <ul>
+                <li>Jan</li>
+                <li>2023</li>
+              </ul>
+            </Col>
+            <Col>
+              <p>Monday</p>
+            </Col>
+          </Row>
+        </Container>
+      </Row>
+      <Row className="credits-area"></Row>
     </Container>
   );
 }
