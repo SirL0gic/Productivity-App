d95b6889711db6e9573c98103553ae0895ea821b
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-13 12:33:29 +0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
+
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -13,28 +15,47 @@ import "./App.css";
 function App() {
   return (
     <Container fluid className="App">
+
       <Row className="title-area">
         <h1>&#128221; To Do List in React &#128221;</h1>
       </Row>
+
       <Row className="list-area">
+
         <Container>
           <Row className="date-time">
+
             <Col>
               <p>1</p>
             </Col>
+
             <Col>
               <ul>
                 <li>Jan</li>
                 <li>2023</li>
               </ul>
             </Col>
+
             <Col>
               <p>Monday</p>
             </Col>
+
           </Row>
+
+          <Row className="task-tems">
+            <p>item1</p>
+          </Row>
+
+          <Row className="button-area">
+      <Button variant="primary">Primary</Button>{' '}
+        </Row>
+
         </Container>
+
       </Row>
-      <Row className="credits-area"></Row>
+
+   
+
     </Container>
   );
 }
