45e59cc6c2cbe9c6c5666563cc8b60a226eb998c
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-16 16:25:26 +0400

    update

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -5,7 +5,7 @@ import "../task.css";
 let Task = () => {
     return (
         <div className="checklist">
-        <input classname="" id="01" type="checkbox" name="r" value="1"/>
+        <input className="check" id="01" type="checkbox" name="r" value="1"/>
         <label for="01">Bread</label>
         <button className="edit-button"><img className="edit-image" alt='editimage' src="https://img.icons8.com/nolan/96/edit--v1.png"/></button>
         <button className="del-button"><img className="del-image" alt='delimage' src="https://img.icons8.com/color/96/null/trash--v2.png"/></button>
@@ -15,7 +15,5 @@ let Task = () => {
 };
 
 export default Task;
-
-
 // 5-7 days 
 
