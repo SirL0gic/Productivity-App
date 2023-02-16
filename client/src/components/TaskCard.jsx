f2fe1554633aab3f33dcd246e6f03513f9690e0f
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-16 15:26:45 +0400

    update

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -8,7 +8,7 @@ let Task = () => {
         <input id="01" type="checkbox" name="r" value="1"/>
         <label for="01">Bread</label>
         <button className="edit-button"><img className="edit-image" alt='editimage' src="https://img.icons8.com/nolan/96/edit--v1.png"/></button>
-        <button className="del-button"><img className="del-image" alt='delimage' src="https://img.icons8.com/nolan/96/del-key.png"/></button>
+        <button className="del-button"><img className="del-image" alt='delimage' src="https://img.icons8.com/color/96/null/trash--v2.png"/></button>
       </div>
     )
 
