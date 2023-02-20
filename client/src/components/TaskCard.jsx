e7cbf32f82354a46db48590127f82da588b29299
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-20 10:58:43 +0400

    update

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -5,11 +5,20 @@ import "../task.css";
 let Task = () => {
     return (
         <div className="checklist">
-        <input className="check" id="01" type="checkbox" name="r" value="1"/>
+        <input className="checkbox-rect2" id="01" type="checkbox" name="r" value="1"/>
         <label for="01">Bread</label>
         <button className="edit-button"><img className="edit-image" alt='editimage' src="https://img.icons8.com/nolan/96/edit--v1.png"/></button>
         <button className="del-button"><img className="del-image" alt='delimage' src="https://img.icons8.com/color/96/null/trash--v2.png"/></button>
+
+        
+      <div class="checkbox-rect2">
+        <input type="checkbox" id="checkbox-rect2" name="check"/>
+        <label for="checkbox-rect2">Check one</label>
       </div>
+   
+      </div>
+
+      
     )
 
 };
