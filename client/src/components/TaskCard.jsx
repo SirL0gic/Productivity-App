0aecb5d2b4afa348972fb53aa531a5fe86685fb3
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-20 13:21:10 +0400

    Update TaskCard.jsx

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 //Styles
 import "../task.css";
 
+
+
 let Task = () => {
+
+  const [isChecked,setIsChecked] = useState(false);
+
+function handleCheckboxChange(e) {
+  setIsChecked(e.target.checked);
+}
+
     return (
         <div className="checklist">
-          <input className="check" id="ch" type="checkbox" />
+          <input className="check" id="ch" type="checkbox"  checked={isChecked} onChange={handleCheckboxChange} />
           <label className="taskname">Bread</label>
           <button className="edit-button"><img className="edit-image" alt='editimage' src="https://img.icons8.com/nolan/96/edit--v1.png"/></button>
           <button className="del-button"><img className="del-image" alt='delimage' src="https://img.icons8.com/color/96/null/trash--v2.png"/></button>
+        
         </div>
     )
 
