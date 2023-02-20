101526f128cc8cb0a5e191ad3a2d4f78208d65a4
Abis <63680552+SirL0gic@users.noreply.github.com>
2023-02-20 11:04:37 +0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
-import Button from 'react-bootstrap/Button';
-
+import Button from "react-bootstrap/Button";
 
 //Bootstrap-Bundle-JS-CSS
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -17,16 +16,13 @@ import Task from "./components/TaskCard";
 function App() {
   return (
     <Container fluid className="App">
-
       <Row className="title-area">
         <h1 className="text-pop-up-top">&#128221; To Do List &#128221;</h1>
       </Row>
 
       <Row className="list-area">
-
         <Container>
           <Row className="date-time">
-
             <Col className="col-day">
               <p>1</p>
             </Col>
@@ -41,27 +37,19 @@ function App() {
             <Col>
               <p className="day-area">Monday</p>
             </Col>
-
           </Row>
 
           <Row className="task-items">
-            <Task/>
-            <Task/>
-            <Task/>
-            
+            <Task />
+            <Task />
+            <Task />
           </Row>
 
-
           <Row className="button-area">
-      <Button variant="primary">New Task</Button>
-        </Row>
-
+            <Button variant="primary">New Task</Button>
+          </Row>
         </Container>
-
       </Row>
-
-   
-
     </Container>
   );
 }
